9729da5546bd25f8c62a57b8044142d76f84f57a
LoncaricLaura <laura.loncaric032@gmail.com>
2023-01-12 22:21:51 +0100

    WT1 and WT2 finished

---
+++ projekt-01/WT1.py
@@ -0,0 +1,30 @@
+import aiosqlite
+import asyncio
+import aiohttp
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.post("/")
+async def get_data(request):
+    try:
+        data = await request.json()
+        res = []
+        wt1 = [i for i in data if i["username"].lower().startswith("w")]
+        if (wt1):
+            res.append(wt1)
+            print(res)
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://127.0.0.1:8084/gatherdata", json=res) as res:
+                    result = await res.json()
+                    print("Send!")
+        return web.json_response({"status":"ok", "data_receive":res, "result": result.get("data")}, status=200) 
+    except Exception as e:
+        return web.json_response({"failed":str(e)}, status=500) 
+
+
+app = web.Application()
+
+app.router.add_routes(routes)
+
+web.run_app(app, port=8082)
